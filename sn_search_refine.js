c801b8c84514f367b662b69963affa21e1916082
Justin <justin.hellings@gmail.com>
2012-08-22 13:33:24 +0100

    Modification and fixes to search refine form.

--- sn_search_refine.js
+++ sn_search_refine.js
@@ -15,7 +15,7 @@ jQuery(document).ready(
 
 function on_form_option_selection( event ) {
 
-  var url = get_ajax_url_from_form_selections();
+  var url = get_ajax_url_from_form_selections( event );
   load_and_apply_ajax_data( url );
 }
 
@@ -37,39 +37,55 @@ function get_ajax_url_from_current_url() {
 }
 
 
-function get_ajax_url_from_form_selections() {
+function get_ajax_url_from_form_selections( event ) {
 
-  var continent_value = jQuery(
-      '#-sn-search-refine-form #edit-continents option:selected').val();
-  var continent_id = get_id_from_option_value( continent_value );
+  //console.debug( event );
 
-  var country_value = jQuery(
-      '#-sn-search-refine-form #edit-countries option:selected').val();
-  var country_id = get_id_from_option_value( country_value );
+  var url = 'http://' + window.location.hostname + '/ajax';
 
-  var region_ids = [];
-  jQuery('#-sn-search-refine-form input.form-checkbox:checked').each(
-      function( idx, cb ) { region_ids.push( cb.value ); } );
 
   var path_components = window.location.pathname.split('/');
-
   // Get rid of any zero-length path components at start of path
   // (e.g. because of a leading '/' in the pathname).
   while ( path_components.length > 0 && path_components[0].length == 0 ) {
     path_components.shift();
   }
-
   // if ( path_components.length == 0 ) { panic!; }
-
   var search_name = path_components[0];
+  url += '/' + search_name;
 
-  var url = 'http://' + window.location.hostname +
-    '/' + 'ajax' +
-    '/' + search_name +
-    '/' + continent_id +
-    '/' + country_id +
-    '/' + region_ids.join(',');
-  //console.debug( url );
+
+  var continent_value = jQuery(
+      '#-sn-search-refine-form #edit-continents option:selected').val();
+  var continent_id = null;
+  if ( continent_value ) {
+    continent_id = get_id_from_option_value( continent_value );
+    url += '/' + continent_id;
+  }
+  if ( ! continent_value  ||  event.target.id == 'edit-continents' ) {
+    return url;
+  }
+
+
+  var country_value = jQuery(
+      '#-sn-search-refine-form #edit-countries option:selected').val();
+  var country_id = null;
+  if ( country_value ) {
+    country_id = get_id_from_option_value( country_value );
+    url += '/' + country_id;
+  }
+  if ( ! country_value  ||  event.target.id == 'edit-countries' ) {
+    return url;
+  }
+
+  var region_ids = [];
+  jQuery('#-sn-search-refine-form input.form-checkbox:checked').each(
+      function( idx, cb ) { region_ids.push( cb.value ); } );
+
+  if ( region_ids.length > 0 ) {
+    url += '/' + region_ids.join(',');
+  }
+
   return url;
 }
 
@@ -111,66 +127,3 @@ function get_id_from_option_value( value ) {
   var matches = pattern.exec( value );
   return matches.length > 1 ? matches[1] : NULL;
 }
-
-function hide_invalid_select_options() {
-  //var continent_id = get_continent_tid();
-  //jQuery('#-sn-search-refine-form #edit-countries option')
-  //  .each( function( idx, option ) {
-  //      if ( option ) {
-  //      console.debug( 'idx...' );
-  //      console.debug( idx );
-  //      console.debug( 'option...' );
-  //      console.debug( option );
-  //      console.debug( 'this...' );
-  //      console.debug( this );
-  //      var ids = get_id_from_option_value( option.value );
-  //      if ( ids.parent_tid != continent_id ) {
-  //      jQuery( option ).addClass( '.ui-helper-hidden' );
-  //      }
-  //      }
-  //      }
-  //      );
-}
-/**
- * Version 1
- * Added exposed region filter to AJAX view and manipulated it through JS.
- *
- * Didn't work because the exposed region filter clashed with the
- * contextual region filter. A node would have to have had two regions.
- *
- *
-function set_option_selection( region_id, do_select ) {
-  var view_option_path =
-    '#views-exposed-form-search-hotels-page select[name="field_region_tid[]"] option[value="' + region_id + '"]';
-
-  jQuery( view_option_path ).selected( do_select );
-}
-
-function submit_ajax_form() {
-  jQuery('#views-exposed-form-search-hotels-page .form-submit').click();
-}
-
-
-jQuery(document).ready( function() {
-
-    // The view loads with no regions selected in the region filter.
-    // Set the same selections as the search-form region checkboxes.
-    var checked_boxes =
-    jQuery('#-sn-search-refine-form input.form-checkbox:checked');
-
-    checked_boxes.each( function( x, cb ) {
-      var region_id = cb.value;
-      set_option_selection( region_id, true );
-      } );
-
-    // Add a click event to the search-form checkboxes so that
-    // they change selected regions in the view and reload it.
-    jQuery('#-sn-search-refine-form input.form-checkbox').click(
-      function( event ) {
-      var region_id = event.target.value;
-      var is_selected = event.target.checked;
-      set_option_selection( region_id, is_selected );
-      submit_ajax_form();
-      } );
-    } );
-*/
