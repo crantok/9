590685789eee850b9f7cd5dff16c0509179a9e11
Justin <justin.hellings@gmail.com>
2012-08-25 14:48:43 +0100

    Added boolean field checkboxes to search refine form.

--- sn_search_refine.js
+++ sn_search_refine.js
@@ -15,6 +15,7 @@ jQuery(document).ready(
 
 function on_form_option_selection( event ) {
 
+  console.debug( 'on_form_selection' );
   var url = get_ajax_url_from_form_selections( event );
   load_and_apply_ajax_data( url );
 }
@@ -55,40 +56,54 @@ function get_ajax_url_from_form_selections( event ) {
   url += '/' + search_name;
 
 
-  var continent_value = jQuery(
-      '#-sn-search-refine-form #edit-continents option:selected').val();
-  var continent_id = null;
-  if ( continent_value ) {
-    continent_id = get_id_from_option_value( continent_value );
-    url += '/' + continent_id;
-  }
-  if ( ! continent_value  ||  event.target.id == 'edit-continents' ) {
-    return url;
-  }
+  url = add_selected_term_id_to_url( url, 'edit-continents' );
+  url = add_selected_term_id_to_url( url, 'edit-countries' );
 
-
-  var country_value = jQuery(
-      '#-sn-search-refine-form #edit-countries option:selected').val();
-  var country_id = null;
-  if ( country_value ) {
-    country_id = get_id_from_option_value( country_value );
-    url += '/' + country_id;
-  }
-  if ( ! country_value  ||  event.target.id == 'edit-countries' ) {
-    return url;
-  }
 
   var region_ids = [];
-  jQuery('#-sn-search-refine-form input.form-checkbox:checked').each(
+  jQuery('#-sn-search-refine-form #edit-regions input.form-checkbox:checked').each(
       function( idx, cb ) { region_ids.push( cb.value ); } );
-
+//console.debug( region_ids );
   if ( region_ids.length > 0 ) {
     url += '/' + region_ids.join(',');
   }
+  else {
+    url += '/all';
+  }
+
+
+  // Important to know order of boolean values in the view's contextual
+  // filters. Currently this is alphabetical, i.e.
+  //     eco, family, honeymoon, luxury
+
+  url = add_boolean_to_url( url, 'edit-eco' );
+  url = add_boolean_to_url( url, 'edit-family' );
+  url = add_boolean_to_url( url, 'edit-honeymoon' );
+  url = add_boolean_to_url( url, 'edit-luxury' );
+
+  console.debug( url );
 
   return url;
 }
 
+function add_selected_term_id_to_url( url, select_id ) {
+  var select_value = jQuery(
+      '#-sn-search-refine-form #' + select_id + ' option:selected'
+      ).val();
+console.debug( select_value );
+  if ( select_value ) {
+    term_id = get_id_from_option_value( select_value );
+    return url + '/' + term_id;
+  }
+  else {
+    return url + '/all';
+  }
+}
+
+function add_boolean_to_url( url, checkbox_id ) {
+  var checkbox = jQuery( '#-sn-search-refine-form #' + checkbox_id )
+  return url + '/' + ( checkbox.attr('checked') ? checkbox.val() : 'all' );
+}
 
 function load_and_apply_ajax_data( url, do_replace_view ) {
 
@@ -102,8 +117,7 @@ function load_and_apply_ajax_data( url, do_replace_view ) {
 
       // Replace old view and form with newly retrieved ones.
       if ( do_replace_view ) {
-      jQuery( '#content > .view' )
-      .removeClass().addClass('view').html( data.view );
+      jQuery( '#content > .view' ).replaceWith( data.view );
       }
       jQuery( '#ajax-sn-search-refine-form' ).html( data.form );
 
